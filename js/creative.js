de04aecbddf8a2fab4883fa71a72d913ffd884f4
Gary Ritchie <gary@lyonritchie.com>
2020-04-03 12:52:47 -0400

    Function for scaling larger h2 in header

--- js/creative.js
+++ js/creative.js
@@ -4,45 +4,54 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-(function($) {
-    "use strict"; // Start of use strict
+(function ($) {
+  "use strict"; // Start of use strict
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $("a.page-scroll").bind("click", function (event) {
+    var $anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top - 50,
+        },
+        1250,
+        "easeInOutExpo"
+      );
+    event.preventDefault();
+  });
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    })
+  // Highlight the top nav as scrolling occurs
+  $("body").scrollspy({
+    target: ".navbar-fixed-top",
+    offset: 51,
+  });
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
+  // Closes the Responsive Menu on Menu Item Click
+  $(".navbar-collapse ul li a").click(function () {
+    $(".navbar-toggle:visible").click();
+  });
 
-    // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
+  // Fit Text Plugin for Main Header
+  $("h1#fit").fitText(1.2, {
+    minFontSize: "35px",
+    maxFontSize: "65px",
+  });
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+  // Fit Text Plugin for Secondary Header
+  $("h2#fit").fitText(1.2, {
+    minFontSize: "35px",
+    maxFontSize: "65px",
+  });
 
-    // Initialize WOW.js Scrolling Animations
-    new WOW().init();
+  // Offset for Main Navigation
+  $("#mainNav").affix({
+    offset: {
+      top: 100,
+    },
+  });
 
+  // Initialize WOW.js Scrolling Animations
+  new WOW().init();
 })(jQuery); // End of use strict
